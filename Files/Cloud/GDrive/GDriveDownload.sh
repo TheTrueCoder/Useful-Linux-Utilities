58f5db35599d17da11b7f9cbeb0dd56ed5713923
TheTrueCoder <38576090+TheTrueCoder@users.noreply.github.com>
2020-09-21 19:29:19 +1200

    Create GDriveDownload.sh
    GDriveDownload.sh can download public google drive files.

---
+++ Files/Cloud/GDrive/GDriveDownload.sh
@@ -0,0 +1,4 @@
+FILEID=$1
+FILENAME=$2
+#Credit for command goes to vast.ai faq
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
